7e3b41c544bafe4d0a14a8982fe92af4f6504cc1
priteem <89594900+priteem@users.noreply.github.com>
2021-11-16 09:31:54 +0530

    Add files via upload

---
+++ src/app/app.component.header.ts.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <p>
+     Workshop statement
+    </p>
+  `,
+  styles: []
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
